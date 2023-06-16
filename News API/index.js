b67bfff8dc62cb67751d6e75d15bfdfb64f12051
MichaelFilbert <90560015+MichaelFilbert@users.noreply.github.com>
2023-06-16 15:31:38 +0700

    Add files via upload

---
+++ News API/index.js
@@ -0,0 +1,93 @@
+// variables
+const newsType = document.getElementById("newsType");
+const newsdetails = document.getElementById("newsdetails");
+require('dotenv').config();
+
+
+// Array
+var newsDataArr = [];
+
+// apis 
+const API_KEY = process.env.API_NEWS;
+const HEADLINES_NEWS = "https://newsapi.org/v2/everything?qInTitle='sign language'&sortBy=publishedAt&pageSize=4&apiKey=";
+
+
+window.onload = function() {
+    newsType.innerHTML="<h4>Sign Language</h4>";
+    fetchHeadlines();
+};
+
+const fetchHeadlines = async () => {
+    const response = await fetch(HEADLINES_NEWS+API_KEY);
+    newsDataArr = [];
+    if(response.status >=200 && response.status < 300) {
+        const myJson = await response.json();
+        newsDataArr = myJson.articles;
+    } else {
+        // handle errors
+        console.log(response.status, response.statusText);
+        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        return;
+    }
+
+    displayNews();
+}
+
+function displayNews() {
+
+    newsdetails.innerHTML = "";
+
+    if(newsDataArr.length == 0) {
+        newsdetails.innerHTML = "<h5>No data found.</h5>"
+        return;
+    }
+
+    newsDataArr.forEach(news => {
+
+        var date = news.publishedAt.split("T");
+        
+        var col = document.createElement('div');
+        col.className="col-sm-12 col-md-4 col-lg-3 p-2 card";
+
+        var card = document.createElement('div');
+        card.className = "p-2";
+
+        var image = document.createElement('img');
+        image.setAttribute("height","matchparent");
+        image.setAttribute("width","100%");
+        image.src=news.urlToImage;
+
+        var cardBody = document.createElement('div');
+        
+        var newsHeading = document.createElement('h5');
+        newsHeading.className = "card-title";
+        newsHeading.innerHTML = news.title;
+
+        var dateHeading = document.createElement('h6');
+        dateHeading.className = "text-primary";
+        dateHeading.innerHTML = date[0];
+
+        var discription = document.createElement('p');
+        discription.className="text-muted";
+        discription.innerHTML = news.description;
+
+        var link = document.createElement('a');
+        link.className="btn btn-dark";
+        link.setAttribute("target", "_blank");
+        link.href = news.url;
+        link.innerHTML="Read more";
+
+        cardBody.appendChild(newsHeading);
+        cardBody.appendChild(dateHeading);
+        cardBody.appendChild(discription);
+        cardBody.appendChild(link);
+
+        card.appendChild(image);
+        card.appendChild(cardBody);
+
+        col.appendChild(card);
+
+        newsdetails.appendChild(col);
+    });
+
+}
